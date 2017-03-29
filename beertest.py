7d29c5badaae10c8ef28c9d5e60b993e180bfcf0
Lisa Yau <lisayau@Lisas-MacBook-Pro.local>
2017-03-29 02:01:18 -0400

    added route to /menurecs

--- beertest.py
+++ beertest.py
@@ -28,8 +28,8 @@ def recs(df,numrecs, weights):
     return reclist2
 
 def getrecs(userbeer,df):
-    userdocs = dfbeers[dfbeers['beer']=='shock top belgian white']
-    not_userbeer = dfbeers.loc[dfbeers['beer'] != userbeer]
+    userdocs = df[df['beer']== userbeer]
+    not_userbeer = df.loc[df['beer'] != userbeer]
     bmatrix = not_userbeer.ix[:,7:].copy()
     usermatrix = userdocs.ix[:,7:].copy()
     distance = pd.DataFrame(cosine_similarity(bmatrix,usermatrix))
@@ -49,6 +49,34 @@ def getrecs(userbeer,df):
     closestbeers = recs(beer_distances,num_recs,weightsdict)
     return closestbeers
 
+def getmenurecs(userbeer, menu, df):
+    userdocs = df[df['beer']== userbeer.lower()]
+    #not_userbeer = df.loc[df['beer'] != userbeer]
+    usermenu = [i.lower() for i in menu]
+    menudocs = pd.DataFrame()
+    for i in range(len(usermenu)):
+        menudocs = pd.concat([menudocs, df[df['beer']==usermenu[i]]])
+    #pd.concat([df_a, df_b])
+
+    bmatrix = menudocs.ix[:,7:].copy()
+    usermatrix = userdocs.ix[:,7:].copy()
+    distance = pd.DataFrame(cosine_similarity(bmatrix,usermatrix))
+    #join distances back to original dataframe with all beers (beside test beer)
+    menudocs.reset_index(inplace=True)
+    distance.reset_index(inplace=True)
+    beer_distances = menudocs.join(distance, how='left', lsuffix='_l')
+    beer_distances = beer_distances.rename(columns ={beer_distances.columns[-1]:'distance'})
+    beer_distances.drop(['index_l','index'], axis=1, inplace=True)
+    beer_distances['same_style']= beer_distances['beerstyle'] == str(userdocs.beerstyle).split()[1]
+    def inABVrange(df):
+        return ((float(str(userdocs['abv']).split()[1]))-2.2 <= df['abv']) and ((float(str(userdocs['abv']).split()[1]))+2.2 >= df['abv'])
+    beer_distances['abv_tier'] = beer_distances.apply(inABVrange, axis=1)
+
+    weightsdict = {'abv':.15,'rating':.3,'samestyle':0,'text':.55}
+    num_recs = 3
+    closestbeers = recs(beer_distances,num_recs,weightsdict)
+    return closestbeers
+
 #loads the page
 @app.route("/")
 def viz_page():
@@ -56,7 +84,7 @@ def viz_page():
         return viz_file.read()
     
 #listens
-@app.route("/gof", methods=["POST"])
+@app.route("/recs", methods=["POST"])
 def score():
     """
     When A POST request with json data is made to this url,
@@ -68,7 +96,18 @@ def score():
     result = getrecs(userbeer, dfbeers) #get list of recs from previously defined function
     return flask.jsonify({'words': result})
 
-
+@app.route("/menurecs", methods=["POST"])
+def score2():
+    """
+    When A POST request with json data is made to this url,
+    Read the grid from the json, update and send it back
+    """
+    #html "posts" a request and python gets the json  from that request 
+    data = flask.request.json
+    userbeer = data['grid'][0] #get the user input beer, do something with it in the model
+    #menu = ???
+    result = getmenurecs(userbeer, menu, dfbeers) #get list of recs from previously defined function
+    return flask.jsonify({'words': result})
 
 
 
